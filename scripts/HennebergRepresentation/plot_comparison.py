60b7baac4d4e19f6b7e467553a2a57d5c43a7d74
Antoine Baillod <antoine.baillod@epfl.ch>
2022-12-05 22:05:59 +0100

    update

--- scripts/HennebergRepresentation/plot_comparison.py
+++ scripts/HennebergRepresentation/plot_comparison.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 plt.rcParams['text.usetex'] = True
 
-ii = 3
+ii = 1
 
 
 bean = ii==1
@@ -78,7 +78,7 @@ if ellipse:
     dbs_lb0 = py_spec.SPECout('RotatingEllipse_Lb0_Lf2.sp.h5')
     dbs_lb1 = py_spec.SPECout('RotatingEllipse_Lb1_Lf2.sp.h5')
     
-    fig,ax = plt.subplots(1,3,figsize=(12,4))
+    fig,ax = plt.subplots(1,3,figsize=(8,8))
     dbs_lb0.plot_poincare(ax=ax[0],prange='upper',facecolor='k')
     dbs_lb1.plot_poincare(ax=ax[0],prange='lower',facecolor='k')
     dbs_lb0.plot_kam_surface(ax=ax[0])
@@ -87,7 +87,7 @@ if ellipse:
     ax[0].set_ylabel(r'$Z$[m]', fontsize=ft)
     ax[0].set_xlim((8.25,11.75))
     ax[0].tick_params(axis='both', labelsize=ft)
-    
+ 
     dbs_lb1.plot_iota(ax=ax[1],marker='x')
     dbs_lb0.plot_iota(ax=ax[1],marker='o',facecolor='none',edgecolor='r')
     #ax[1].set_ylim((3,5.5))
@@ -104,33 +104,43 @@ if ellipse:
     ax[2].tick_params(axis='both', labelsize=ft)
 
 if w7x:
+    import numpy as np
     dbs_lb0 = py_spec.SPECout('W7X_Nvol2_Lb0_Lf2.sp.h5')
     dbs_lb1 = py_spec.SPECout('W7X_Nvol2_Lb1_Lf2.sp.h5')
     
-    fig,ax = plt.subplots(1,3,figsize=(12,4))
-    dbs_lb0.plot_poincare(ax=ax[0],prange='upper',facecolor='k')
-    dbs_lb1.plot_poincare(ax=ax[0],prange='lower',facecolor='k')
-    dbs_lb0.plot_kam_surface(ax=ax[0])
-    dbs_lb1.plot_kam_surface(ax=ax[0],c='b')
-    ax[0].set_xlabel(r'$R$[m]', fontsize=ft)
-    ax[0].set_ylabel(r'$Z$[m]', fontsize=ft)
-    ax[0].set_xlim((5.5,6.3))
-    ax[0].tick_params(axis='both', labelsize=ft)
+    fig,ax = plt.subplots(2,2,figsize=(10,8))
+    dbs_lb0.plot_poincare(ax=ax[0,0],prange='upper',facecolor='k')
+    dbs_lb1.plot_poincare(ax=ax[0,0],prange='lower',facecolor='k')
+    dbs_lb0.plot_kam_surface(ax=ax[0,0])
+    dbs_lb1.plot_kam_surface(ax=ax[0,0],c='b')
+    ax[0,0].set_xlabel(r'$R$[m]', fontsize=ft)
+    ax[0,0].set_ylabel(r'$Z$[m]', fontsize=ft)
+    ax[0,0].set_xlim((5.5,6.3))
+    ax[0,0].tick_params(axis='both', labelsize=ft)
+ 
+    dbs_lb0.plot_poincare(ax=ax[0,1],toroidalIdx=24,prange='upper',facecolor='k')
+    dbs_lb1.plot_poincare(ax=ax[0,1],toroidalIdx=48,prange='lower',facecolor='k')
+    dbs_lb0.plot_kam_surface(ax=ax[0,1],zeta=np.pi/5)
+    dbs_lb1.plot_kam_surface(ax=ax[0,1],zeta=np.pi/5,c='b')
+    ax[0,1].set_xlabel(r'$R$[m]', fontsize=ft)
+    ax[0,1].set_ylabel(r'$Z$[m]', fontsize=ft)
+    ax[0,1].set_xlim((4.6,6))
+    ax[0,1].tick_params(axis='both', labelsize=ft)
     
-    dbs_lb1.plot_iota(ax=ax[1],marker='x')
-    dbs_lb0.plot_iota(ax=ax[1],marker='o',facecolor='none',edgecolor='r')
-    ax[1].set_ylim((3,5.5))
-    ax[1].set_xlabel(r'$R$[m]', fontsize=ft)
-    ax[1].set_ylabel(r'$\iota$',fontsize=ft+2)
-    ax[2].tick_params(axis='both', labelsize=ft)
+    dbs_lb1.plot_iota(ax=ax[1,0],marker='x')
+    dbs_lb0.plot_iota(ax=ax[1,0],marker='o',facecolor='none',edgecolor='r')
+    ax[1,0].set_ylim((-1,-0.6))
+    ax[1,0].set_xlabel(r'$R$[m]', fontsize=ft)
+    ax[1,0].set_ylabel(r'$\iota$',fontsize=ft+2)
+    ax[1,0].tick_params(axis='both', labelsize=ft)
     
     
-    ax[2].plot(dbs_lb1.iterations['ForceErr'],c='b')
-    ax[2].plot(dbs_lb0.iterations['ForceErr'],c='r')
-    ax[2].set_yscale('log')
-    ax[2].set_xlabel(r'$N_{eval}$', fontsize=ft)
-    ax[2].set_ylabel(r'$|f|$', fontsize=ft)
-    ax[2].tick_params(axis='both', labelsize=ft)
+    ax[1,1].plot(dbs_lb1.iterations['ForceErr'],c='b')
+    ax[1,1].plot(dbs_lb0.iterations['ForceErr'],c='r')
+    ax[1,1].set_yscale('log')
+    ax[1,1].set_xlabel(r'$N_{eval}$', fontsize=ft)
+    ax[1,1].set_ylabel(r'$|f|$', fontsize=ft)
+    ax[1,1].tick_params(axis='both', labelsize=ft)
 
 
 
