837af435d7c9bbdc0578825b9f78bb651e77c063
Antoine Baillod <antoine.baillod@epfl.ch>
2023-01-03 12:53:57 +0100

    update

--- data/plot_hdi_vs_energy.py
+++ data/plot_hdi_vs_energy.py
@@ -44,13 +44,44 @@ ax.set_xlabel('kWh per inhabitant')
 
 
 # France
-#ii = 39
-#ax.annotate(country[ii], xy=(np.log10(energy[ii]), hdi[ii]),
-#            xytext=(4, 0.9),
-#            arrowprops={"arrowstyle":"->", "color":"gray"},
-#            )
+ii = 12
+data_eca = data_eca.to_numpy()
+ax.annotate(data_eca[ii,0], xy=(np.log10(data_eca[ii,1]*11.63),
+            data_eca[ii,3]),
+            color='#4C6A9C',
+            xytext=(3.75, 0.9),
+            arrowprops={"arrowstyle":"-", "color":'#4C6A9C'},
+            )
 
+# Chine
+ii = 2
+data_ea = data_ea.to_numpy()
+ax.annotate(data_ea[ii,0], xy=(np.log10(data_ea[ii,1]*11.63),
+            data_ea[ii,3]),
+            color='#A2559C',
+            xytext=(5, 0.65),
+            arrowprops={"arrowstyle":"-", "color":'#A2559C'},
+            )
 
+# Niger
+ii = 13
+data = data_ss.to_numpy()
+ax.annotate(data[ii,0], xy=(np.log10(data[ii,1]*11.63),
+            data[ii,3]),
+            color='#8C4569',
+            xytext=(3, 0.5),
+            arrowprops={"arrowstyle":"-", "color":'#8C4569'},
+            )
+
+# Argentina
+ii = 13
+data = data_la.to_numpy()
+ax.annotate(data[ii,0], xy=(np.log10(data[ii,1]*11.63),
+            data[ii,3]),
+            color='#883039',
+            xytext=(3, 0.78),
+            arrowprops={"arrowstyle":"-", "color":'#8C4569'},
+            )
 
 plt.tight_layout()
 plt.show()
